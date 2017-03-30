4b11c82b96076bdfb7bc42a6553c3599c6055113
Xotic750 <xotic750@gmail.com>
2017-03-30 10:22:27 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,71 @@
+<a name="module_find-last-index-x"></a>
+
+## find-last-index-x
+<a href="https://travis-ci.org/Xotic750/find-last-index-x"
+title="Travis status">
+<img
+src="https://travis-ci.org/Xotic750/find-last-index-x.svg?branch=master"
+alt="Travis status" height="18">
+</a>
+<a href="https://david-dm.org/Xotic750/find-last-index-x"
+title="Dependency status">
+<img src="https://david-dm.org/Xotic750/find-last-index-x.svg"
+alt="Dependency status" height="18"/>
+</a>
+<a
+href="https://david-dm.org/Xotic750/find-last-index-x#info=devDependencies"
+title="devDependency status">
+<img src="https://david-dm.org/Xotic750/find-last-index-x/dev-status.svg"
+alt="devDependency status" height="18"/>
+</a>
+<a href="https://badge.fury.io/js/find-last-index-x" title="npm version">
+<img src="https://badge.fury.io/js/find-last-index-x.svg"
+alt="npm version" height="18">
+</a>
+
+This method returns the index of the first element in the array that satisfies the
+provided testing function. Otherwise -1 is returned.
+
+Requires ES3 or above.
+
+**Version**: 1.0.0  
+**Author**: Xotic750 <Xotic750@gmail.com>  
+**License**: [MIT](&lt;https://opensource.org/licenses/MIT&gt;)  
+**Copyright**: Xotic750  
+<a name="exp_module_find-last-index-x--module.exports"></a>
+
+### `module.exports` ⇒ <code>number</code> ⏏
+Like `findIndex`, this method returns an index in the array, if an element
+in the array satisfies the provided testing function. Otherwise -1 is returned.
+
+**Kind**: Exported member  
+**Returns**: <code>number</code> - Returns index of positively tested element, otherwise -1.  
+**Throws**:
+
+- <code>TypeError</code> If array is `null` or `undefined`-
+- <code>TypeError</code> If `callback` is not a function.
+
+
+| Param | Type | Description |
+| --- | --- | --- |
+| array | <code>Array</code> | The array to search. |
+| callback | <code>function</code> | Function to execute on each value in the array,  taking three arguments: `element`, `index` and `array`. |
+| [thisArg] | <code>\*</code> | Object to use as `this` when executing `callback`. |
+
+**Example**  
+```js
+var findLastIndex = require('find-index-x');
+
+function isPrime(element, index, array) {
+  var start = 2;
+  while (start <= Math.sqrt(element)) {
+    if (element % start++ < 1) {
+      return false;
+    }
+  }
+  return element > 1;
+}
+
+console.log(findIndex([4, 6, 8, 12, 14], isPrime)); // -1, not found
+console.log(findIndex([4, 6, 7, 12, 13], isPrime)); // 2
+```
---
+++ tests/spec/test.js
@@ -0,0 +1,119 @@
+/* eslint strict: 1, max-lines: 1, symbol-description: 1, max-nested-callbacks: 1,
+   max-statements: 1 */
+
+/* global JSON:true, expect, module, jasmine, require, describe, it,
+   returnExports */
+
+;(function () { // eslint-disable-line no-extra-semi
+
+  'use strict';
+
+  var findIndex;
+  if (typeof module === 'object' && module.exports) {
+    require('es5-shim');
+    require('es5-shim/es5-sham');
+    if (typeof JSON === 'undefined') {
+      JSON = {};
+    }
+    require('json3').runInContext(null, JSON);
+    require('es6-shim');
+    var es7 = require('es7-shim');
+    Object.keys(es7).forEach(function (key) {
+      var obj = es7[key];
+      if (typeof obj.shim === 'function') {
+        obj.shim();
+      }
+    });
+    findIndex = require('../../index.js');
+  } else {
+    findIndex = returnExports;
+  }
+
+  describe('findIndex', function () {
+    var list = [5, 15, 10, 15, 20];
+
+    it('should find item key by predicate', function () {
+      var result = findIndex(list, function (item) {
+        return item === 15;
+      });
+      expect(result).toBe(1);
+    });
+
+    it('should return -1 when nothing matched', function () {
+      var result = findIndex(list, function (item) {
+        return item === 'a';
+      });
+      expect(result).toBe(-1);
+    });
+
+    it('should throw TypeError when function was not passed', function () {
+      expect(function () {
+        try {
+          findIndex(list);
+        } catch (e) {
+          expect(e).toEqual(jasmine.any(TypeError));
+          throw e;
+        }
+      }).toThrow();
+    });
+
+    it('should receive all three parameters', function () {
+      var foundIndex = findIndex(list, function (value, index, arr) {
+        expect(list[index]).toBe(value);
+        expect(list).toEqual(arr);
+        return false;
+      });
+      expect(foundIndex).toBe(-1);
+    });
+
+    it('should work with the context argument', function () {
+      var context = {};
+      findIndex([1], function () {
+        /* eslint no-invalid-this: 1 */
+        expect(this).toBe(context);
+      }, context);
+    });
+
+    it('should work with an array-like object', function () {
+      var obj = { 0: 1, 1: 2, 2: 3, 3: 2, length: 4 },
+        foundIndex = findIndex(obj, function (item) {
+          return item === 2;
+        });
+      expect(foundIndex).toBe(1);
+    });
+
+    it('should work with an array-like with negative length', function () {
+      var obj = { 0: 1, 1: 2, 2: 3, 3: 2, length: -4 };
+      var foundIndex = findIndex(obj, function () {
+        throw new Error('should not reach here');
+      });
+      expect(foundIndex).toBe(-1);
+    });
+
+    it('should work with a sparse array', function () {
+      var obj = [];
+      var seen = [];
+      obj.length = 3;
+      obj[0] = undefined;
+      obj[2] = 1;
+      expect(1 in obj).toBe(false);
+      var foundIndex = findIndex(obj, function (item, idx) {
+        seen.push([idx, item]);
+        return item === 1 && idx === 2;
+      });
+      expect(foundIndex).toBe(2);
+      expect(seen).toEqual([[0, undefined], [1, undefined], [2, 1]]);
+    });
+
+    it('should work with a sparse array-like object', function () {
+      var obj = { 0: 1, 2: undefined, length: 3.2 };
+      var seen = [];
+      var foundIndex = findIndex(obj, function (item, idx) {
+        seen.push([idx, item]);
+        return false;
+      });
+      expect(foundIndex).toBe(-1);
+      expect(seen).toEqual([[0, 1], [1, undefined], [2, undefined]]);
+    });
+  });
+}());
