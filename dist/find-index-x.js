fb8c9e4085d25d8758f796cdc3a0d663c2235298
Graham Fairweather <xotic750@gmail.com>
2019-07-22 01:04:03 +0200

    :bookmark: v3.0.9
    Auto build

--- dist/find-index-x.js
+++ dist/find-index-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-19T22:14:51.550Z",
+  "date": "2019-07-21T23:03:56.641Z",
   "describe": "",
   "description": "This method returns the index of the first element in the array that satisfies the provided testing function.",
   "file": "find-index-x.js",
   "hash": "816c942027eeae426696",
   "license": "MIT",
-  "version": "3.0.8"
+  "version": "3.0.9"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
